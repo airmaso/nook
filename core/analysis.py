8a4582f0e59e48f676ff856526004f767f9b194f
airmaso <masonlaw.cs@gmail.com>
2026-06-05 18:02:45 -0500

    fix: correct analysis gap logic

--- core/analysis.py
+++ core/analysis.py
@@ -1,18 +1,23 @@
 from core.models import User
 
+def _get_lower_user(u1: User, u2: User) -> User:
+    return u1 if u1.total_points < u2.total_points else u2
+
 def point_gap(u1: User, u2: User) -> int:
     return abs(u2.total_points - u1.total_points)
 
 def puzzle_gap(u1: User, u2: User) -> tuple[float]:
-    min_average = min(u1.average_points, u2.average_points)
+    lo = _get_lower_user(u1, u2)
+
     gap = point_gap(u1, u2)
     return (
-        gap / 563,         # estimate based on max possible score
-        gap / min_average  # estimate using lower ranked user's avg
+        gap / 563,               # estimate based on max possible score
+        gap / lo.average_points  # estimate using lower ranked user's avg
     )
 
 def time_gap(u1: User, u2: User) -> float:
-    min_average_s_per_puzzle = min(u1.average_time, u2.average_time)
+    lo = _get_lower_user(u1, u2)
     avg_puzzles = puzzle_gap(u1, u2)[1]
-    return avg_puzzles * min_average_s_per_puzzle
+
+    return avg_puzzles * lo.average_time
 
