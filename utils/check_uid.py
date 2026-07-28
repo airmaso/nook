f91002da7855dfe56049a226d2f43031926f57ce
lawmaso <masonlaw.cs@gmail.com>
2026-07-27 21:39:27 -0400

    feat: add uid util

---
+++ utils/check_uid.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+from data.scraper import PLAYER_URL
+
+if __name__ == "__main__":
+    lower_bound = 10_839
+    upper_bound = 10_888
+
+    for uid in range(lower_bound, upper_bound + 1):
+        response = requests.get(PLAYER_URL.format(uid=uid))
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        container = soup.find("div", id="container_left")
+        style = container.find_all("style")
+
+        if len(style) != 0:
+            header = container.find("h1", class_="header_font").text.strip()
+            username = header.split("'")[0]
+            print(f"uid={uid} taken by @{username}")
+        else:
+            print(f"uid={uid} is available")
+            break
